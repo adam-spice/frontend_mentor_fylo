07cc25ad00bec2b3e1ead4a6cadde3ad4db6ff41
Adam Spice <spiceybrisket@pop-os.localdomain>
2020-06-03 07:01:56 +1200

    finished

--- src/App.js
+++ src/App.js
@@ -3,7 +3,45 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <main>
+      <div class='left-col'>
+        <img src='./images/logo.svg' id='logo' alt='logo' />
+        <div class='icons'>
+          <div class='icon-back'>
+            <img src='./images/icon-document.svg' alt='document' class='icon' />
+          </div>
+          <div class='icon-back'>
+            <img src='./images/icon-folder.svg' alt='folder' class='icon' />
+          </div>
+          <div class='icon-back'>
+            <img src='./images/icon-upload.svg' alt='upload' class='icon' />
+          </div>
+        </div>
+      </div>
+
+      <div class='right-col'>
+        <h1>
+          You’ve used <span>815 GB</span> of your storage
+        </h1>
+        <div id='bar'>
+          <div>
+            <div id='circle'></div>
+          </div>
+        </div>
+        <div class='bubble'>
+          <div class='bold'>185</div>
+          <div>GB Left</div>
+          <div class='triangle'></div>
+        </div>
+
+        <div id='marking'>
+          <p>0 GB</p>
+          <p>1000 GB</p>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
